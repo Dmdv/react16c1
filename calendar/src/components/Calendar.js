878f4a48ccc5a8dc7e46fd36ce3c74b3ba3723db
dmdv <dima.dyachkov@gmail.com>
2020-11-30 02:00:13 +0300

    Refactored calendar

--- calendar/src/components/Calendar.js
+++ calendar/src/components/Calendar.js
@@ -29,6 +29,51 @@ const CurrentDayHeader = () => {
     </div>;
 }
 
+const DatePickerHeader = () => {
+    return <div className="ui-datepicker-header">
+        <div className="ui-datepicker-title">
+            <span className="ui-datepicker-month">Март</span>&nbsp;<span
+            className="ui-datepicker-year">2017</span>
+        </div>
+    </div>;
+}
+
+const DatesGrid = () => {
+    return <tbody>
+    <tr>
+        <td className="ui-datepicker-other-month">27</td>
+        <td className="ui-datepicker-other-month">28</td>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+    </tr>
+    <tr>
+        <td>6</td>
+        <td>7</td>
+        <td className="ui-datepicker-today">8</td>
+        <td>9</td>
+        <td>10</td>
+        <td>11</td>
+        <td>12</td>
+    </tr>
+    {/*ddd+*/}
+    </tbody>;
+}
+
+const Columns = () => {
+    return <colgroup>
+        <col/>
+        <col/>
+        <col/>
+        <col/>
+        <col/>
+        <col className="ui-datepicker-week-end"/>
+        <col className="ui-datepicker-week-end"/>
+    </colgroup>;
+}
+
 const Calendar = ({props}) => {
 
     const days = [
@@ -44,44 +89,11 @@ const Calendar = ({props}) => {
     return (
         <div className="ui-datepicker">
             <CurrentDayHeader/>
-            <div className="ui-datepicker-header">
-                <div className="ui-datepicker-title">
-                    <span className="ui-datepicker-month">Март</span>&nbsp;<span
-                    className="ui-datepicker-year">2017</span>
-                </div>
-            </div>
+            <DatePickerHeader/>
             <table className="ui-datepicker-calendar">
-                <colgroup>
-                    <col/>
-                    <col/>
-                    <col/>
-                    <col/>
-                    <col/>
-                    <col className="ui-datepicker-week-end"/>
-                    <col className="ui-datepicker-week-end"/>
-                </colgroup>
+                <Columns/>
                 <WeekDays days={days}/>
-                <tbody>
-                <tr>
-                    <td className="ui-datepicker-other-month">27</td>
-                    <td className="ui-datepicker-other-month">28</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
-                    <td>5</td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>7</td>
-                    <td className="ui-datepicker-today">8</td>
-                    <td>9</td>
-                    <td>10</td>
-                    <td>11</td>
-                    <td>12</td>
-                </tr>
-                {/*ddd+*/}
-                </tbody>
+                <DatesGrid/>
             </table>
         </div>
     )
